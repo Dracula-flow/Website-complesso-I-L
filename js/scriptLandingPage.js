ead82095d6dc3207e8210c86cb66a3eee08ef7c8
Dracula-flow <avaglianosimone@gmail.com>
2024-10-25 18:41:17 +0200

    Add files via upload

---
+++ js/scriptLandingPage.js
@@ -0,0 +1,1 @@
+//Script per riempire di contenuto dal db la landing page che porta ai progetti completi. 
---
+++ js/scriptProjects.js
@@ -0,0 +1,2 @@
+//Lo script va associato alla pagina projects
+//Riempie la pagina di card il cui contenuto viene pescato dal db con delle chiamate fetch.
